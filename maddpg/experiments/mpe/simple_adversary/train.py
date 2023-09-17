3a811ecb3185bf6b9a7e387c2cf137c355771b1d
fuhai <yan_fuhai@hotmail.com>
2023-09-18 03:18:23 +0800

    first commit

--- maddpg/experiments/mpe/simple_adversary/train.py
+++ maddpg/experiments/mpe/simple_adversary/train.py
@@ -1,44 +1,54 @@
-import random
-import collections
+import math
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 from pettingzoo.mpe import simple_adversary_v3
+from tqdm import tqdm
 
-from maddpg.model import MADDPG, ReplayBuffer
+from maddpg.model import MADDPG, ReplayBuffer, Transition
 
 
 def evaluate(maddpg, n_episode=10, episode_length=25):
-    # 对学习的策略进行评估,此时不会进行探索
     env = simple_adversary_v3.parallel_env()
-    obs, info = env.reset()
-    returns = np.zeros(len(env.agents))
+    obs, _ = env.reset()
+    returns = {
+        agent_id: 0 for agent_id in env.agents
+    }
     for _ in range(n_episode):
-        if env.agents:
-            for t_i in range(episode_length):
-                actions = maddpg.take_action(env, obs, explore=False)
-                obs, rew, terminated, truncated, info = env.step(actions)
+        if not env.agents:
+            break
 
-                if done:
-                    break
+        for t_i in range(episode_length):
+            actions = maddpg.take_action(env, obs, explore=False)
+            obs, rew, terminated, truncated, _ = env.step(actions)
 
-                rew = np.array(rew)
-                returns += rew / n_episode
+            for agent_id in env.agents:
+                returns[agent_id] += rew[agent_id] / episode_length
 
-    return returns.tolist()
+            if terminated or truncated:
+                break
+
+    return [reward for reward in returns.values()]
 
 
-return_list = []  # 记录每一轮的回报（return）
-total_step = 0
-
+# def plot_result():
+#     return_array = np.array(return_list)
+#     for i, agent_name in enumerate(["adversary_0", "agent_0", "agent_1"]):
+#         plt.figure(
+#         plt.plot(
+#             np.arange(return_array.shape[0]) * 100,
+#             moving_average(return_array[:, i], 9))
+#         plt.xlabel("Episodes")
+#         plt.ylabel("Returns")
+#         plt.title(f"{agent_name} by MADDPG")
+#         plt.legend()
+#         plt.show()
 
 if __name__ == "__main__":
     num_episodes = 5000
     episode_length = 25  # 每条序列的最大长度
-    buffer_size = 100000
+    buffer_size = 10000
     hidden_dim = 64
     actor_lr = 1e-2
     critic_lr = 1e-2
@@ -47,47 +57,84 @@ if __name__ == "__main__":
     batch_size = 1024
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     update_interval = 100
-    minimal_size = 4000
 
     env = simple_adversary_v3.parallel_env()
     observations, info = env.reset()
 
-    replay_buffer = ReplayBuffer(buffer_size)
+    agents = env.agents
+
+    replay_buffer = ReplayBuffer(buffer_size, device=device)
 
     state_dims = dict()
     action_dims = dict()
-    for agent_id, action_space in env.action_spaces.items():
-        action_dims[agent_id] = action_space.n
-    for agent_id, state_space in env.observation_spaces.items():
-        state_dims[agent_id] = state_space.shape[0]
+
+    for agent_id in env.agents:
+        action_dims[agent_id] = env.action_space(agent_id).n
+        state_dims[agent_id] = math.prod(env.observation_space(agent_id).shape)
+
     critic_input_dim = sum(state_dims.values()) + sum(action_dims.values())
 
     maddpg = MADDPG(env, device,
                     actor_lr, critic_lr,
                     hidden_dim, state_dims, action_dims, critic_input_dim,
                     gamma, tau)
+    
+    total_steps = 0
 
-    for i_episode in range(num_episodes):
-        state, info = env.reset()
-        # ep_returns = np.zeros(len(env.agents))
-        for e_i in range(episode_length):
-            actions = maddpg.take_action(env, state, explore=True)
-            next_state, reward, terminated, truncated, _ = env.step(actions)
-            done = terminated or truncated
+    for i in range(50):
+        with tqdm(total=int(num_episodes / 50), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes / 50)):
+                state, info = env.reset()
+                
+                agent_rewards = dict()
+                for i_step in range(episode_length):
+                    if len(env.agents) == 0:
+                        break
 
-            replay_buffer.add(state, actions, reward, next_state, done)
-            state = next_state
+                    actions = maddpg.take_action(env, state, explore=True)
+                    step_actions = {
+                        agent_id: action.argmax(dim=1).item()
+                        for agent_id, action in actions.items()
+                    }
+                    next_state, reward, terminated, truncated, _ = env.step(step_actions)
+                    
+                    for agent_id, reward_ in reward.items():
+                        if agent_id not in agent_rewards:
+                            agent_rewards[agent_id] = []
+                        agent_rewards[agent_id].append(reward_)
 
-            total_step += 1
-            if replay_buffer.size() >= minimal_size and total_step % update_interval == 0:
-                sample = replay_buffer.sample(batch_size)
-                for agent_id in env.agents:
-                    maddpg.update(sample, agent_id)
-                maddpg.update_all_targets()
+                    done = dict()
+                    if env.agents:
+                        done = {
+                            agent_id: terminated[agent_id] or truncated[agent_id]
+                            for agent_id in env.agents
+                        }
+                    else:
+                        done = {agent_id: True for agent_id in agents}
 
-        if (i_episode + 1) % 100 == 0:
-            ep_returns = evaluate(maddpg, n_episode=100)
-            return_list.append(ep_returns)
-            print(f"Episode: {i_episode+1}, {ep_returns}")
+                    replay_buffer.add(Transition(
+                        state, actions, reward, next_state, done))
+                    state = next_state
 
+                    total_steps += 1
+                    if replay_buffer.size() >= batch_size and total_steps % update_interval == 0:
+                        sample = replay_buffer.sample(batch_size, env.agents)
+                        for agent_id in env.agents:
+                            maddpg.optimize(sample, agent_id)
+                        maddpg.update_all_targets()
+
+                if (i_episode + 1) % 10 == 0:
+                    postfix = {
+                        'episode':
+                        '%d' % (num_episodes / 10 * i + i_episode + 1),
+                    }
+                    
+                    for agent_id, rewards in agent_rewards.items():
+                        postfix['reward_%s ' % agent_id] = np.mean(rewards[-10:])
+                    pbar.set_postfix(postfix)
+                    
+                pbar.update(1)
+            
         env.close()
+
+        # plot_result()
--- maddpg/model.py
+++ maddpg/model.py
@@ -1,47 +1,16 @@
 import random
-import collections
+from collections import namedtuple, deque
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 import numpy as np
-import matplotlib.pyplot as plt
+
+from maddpg.utils import onehot_from_logits, sample_gumbel, gumbel_softmax, gumbel_softmax_sample
 
 
-def onehot_from_logits(logits, eps=0.01):
-    ''' 生成最优动作的独热（one-hot）形式 '''
-    argmax_acs = (logits == logits.max(1, keepdim=True)[0]).float()
-    # 生成随机动作,转换成独热形式
-    rand_acs = torch.autograd.Variable(torch.eye(logits.shape[1])[[
-        np.random.choice(range(logits.shape[1]), size=logits.shape[0])
-    ]],
-        requires_grad=False).to(logits.device)
-    # 通过epsilon-贪婪算法来选择用哪个动作
-    return torch.stack([argmax_acs[i] if r > eps else rand_acs[i] for i, r in enumerate(torch.rand(logits.shape[0]))])
-
-
-def sample_gumbel(shape, eps=1e-20, tens_type=torch.FloatTensor):
-    """从Gumbel(0,1)分布中采样"""
-    U = torch.autograd.Variable(tens_type(*shape).uniform_(),
-                                requires_grad=False)
-    return -torch.log(-torch.log(U + eps) + eps)
-
-
-def gumbel_softmax_sample(logits, temperature):
-    """ 从Gumbel-Softmax分布中采样"""
-    y = logits + sample_gumbel(logits.shape, tens_type=type(logits.data)).to(
-        logits.device)
-    return F.softmax(y / temperature, dim=1)
-
-def gumbel_softmax(logits, temperature=1.0):
-    """从Gumbel-Softmax分布中采样,并进行离散化"""
-    y = gumbel_softmax_sample(logits, temperature)
-    y_hard = onehot_from_logits(y)
-    y = (y_hard.to(logits.device) - y).detach() + y
-    return y
-
-
-class Network(nn.Module):
+class Net(nn.Module):
     def __init__(self, num_in, num_out, hidden_dim):
         super().__init__()
         self.linear_stack = nn.Sequential(
@@ -56,52 +25,94 @@ class Network(nn.Module):
         return self.linear_stack(x)
 
 
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'reward', 'next_state', 'done'))
+
+
 class ReplayBuffer:
-    def __init__(self, capacity):
-        self.buffer = collections.deque(maxlen=capacity)
+    def __init__(self, capacity, device="cpu"):
+        self.buffer = deque(maxlen=capacity)
+        self.device = device
 
-    def add(self, state, action, reward, next_state, done):
-        self.buffer.append((state, action, reward, next_state, done))
+    def add(self, transition):
+        self.buffer.append(transition)
 
-    def sample(self, batch_size):
+    def sample(self, batch_size, agents):
         transitions = random.sample(self.buffer, batch_size)
         state, action, reward, next_state, done = zip(*transitions)
-        return np.array(state), action, reward, np.array(next_state), done
+
+        agent_states = self.__to_array(state, agents, batch_size)
+        agent_actions = self.__to_array(action, agents, batch_size)
+        agent_rewards = self.__to_array(reward, agents, batch_size)
+        agent_next_states = self.__to_array(next_state, agents, batch_size)
+        agent_dones = self.__to_array(done, agents, batch_size)
+
+        return agent_states, agent_actions, agent_rewards, agent_next_states, agent_dones
+
+    def __to_tensor(self, x):
+        if isinstance(x, float) or isinstance(x, int) or isinstance(x, bool):
+            return torch.tensor([x], dtype=torch.float).to(device=self.device)
+        return torch.tensor(x, dtype=torch.float).to(device=self.device)
+
+    def __to_array(self, tuples, agents, batch_size):
+        tuple_array = np.array(tuples)
+        agent_array = {
+            agent_id: torch.stack(
+                [self.__to_tensor(tuple_array[i][agent_id])
+                 for i in range(len(tuple_array))],
+                dim=1
+            ).view(batch_size, -1)
+            for agent_id in agents
+        }
+        return agent_array
 
     def size(self):
         return len(self.buffer)
 
 
-class DDPG:
+class Agent:
 
-    def __init__(self, state_dim, action_dim, critic_input_dim, hidden_dim,
-                 actor_lr, critic_lr, device):
-        self.actor = Network(state_dim, action_dim, hidden_dim).to(device)
-        self.target_actor = Network(state_dim, action_dim,
-                                    hidden_dim).to(device)
-        self.critic = Network(critic_input_dim, 1, hidden_dim).to(device)
-        self.target_critic = Network(critic_input_dim, 1,
-                                     hidden_dim).to(device)
+    def __init__(self,
+                 state_dim, action_dim,
+                 critic_input_dim, hidden_dim,
+                 actor_lr=1e-3, critic_lr=1e-3, device='cpu'):
+        self.actor = Net(state_dim, action_dim, hidden_dim).to(device)
+        self.target_actor = Net(state_dim, action_dim,
+                                hidden_dim).to(device)
+
+        self.critic = Net(critic_input_dim, 1, hidden_dim).to(device)
+        self.target_critic = Net(critic_input_dim, 1,
+                                 hidden_dim).to(device)
+
         self.target_critic.load_state_dict(self.critic.state_dict())
         self.target_actor.load_state_dict(self.actor.state_dict())
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),
-                                                lr=actor_lr)
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),
-                                                 lr=critic_lr)
 
-    def take_action(self, state, explore=False):
+        self.actor_optimizer = optim.AdamW(self.actor.parameters(),
+                                           lr=actor_lr)
+        self.critic_optimizer = optim.AdamW(self.critic.parameters(),
+                                            lr=critic_lr)
+
+        self.device = device
+
+    def select_action(self, state, explore=False):
         action = self.actor(state)
         if explore:
-            action = gumbel_softmax(action)
+            action =  gumbel_softmax(action, device=self.device)
         else:
             action = onehot_from_logits(action)
-        return torch.nonzero(action.detach())[0][1].item()
+        return action.detach()
 
-    def soft_update(self, net, target_net, tau):
-        for param_target, param in zip(target_net.parameters(),
-                                       net.parameters()):
-            param_target.data.copy_(param_target.data * (1.0 - tau) +
-                                    param.data * tau)
+    def soft_update(self, tau):
+        self.__sort_update(self.actor, self.target_actor, tau)
+        self.__sort_update(self.critic, self.target_critic, tau)
+
+    def __sort_update(self, policy_net, target_net, tau):
+        policy_net_state_dict = policy_net.state_dict()
+        target_net_state_dict = target_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key] * \
+                tau + target_net_state_dict[key]*(1-tau)
+        target_net.load_state_dict(target_net_state_dict)
 
 
 class MADDPG:
@@ -109,68 +120,128 @@ class MADDPG:
                  state_dims, action_dims, critic_input_dim, gamma, tau):
         self.agents = dict()
         for agent_id in env.agents:
-            self.agents[agent_id] = DDPG(state_dims[agent_id], action_dims[agent_id], critic_input_dim,
-                                         hidden_dim, actor_lr, critic_lr, device)
+            self.agents[agent_id] = Agent(state_dims[agent_id], action_dims[agent_id], critic_input_dim,
+                                          hidden_dim, actor_lr, critic_lr, device)
         self.gamma = gamma
         self.tau = tau
         self.critic_criterion = torch.nn.MSELoss()
         self.device = device
+        self.env = env
+
+        self.action_dims = action_dims
+        self.state_dims = state_dims
 
     @property
-    def policies(self):
-        return [agt.actor for agt in self.agents.values()]
+    def actors(self):
+        return {
+            agent_id: agent.actor for agent_id, agent in self.agents.items()
+        }
 
     @property
-    def target_policies(self):
-        return [agt.target_actor for agt in self.agents.values()]
+    def target_actors(self):
+        return {
+            agent_id: agent.target_actor for agent_id, agent in self.agents.items()
+        }
+
+    def __flatten_as_need(self, t):
+        if t.dim() > 1:
+            return t.flatten()
+        return t
 
     def take_action(self, env, states, explore):
-        states = {
-            agent_id: torch.tensor([states[agent_id]],
-                                   dtype=torch.float, device=self.device)
+        observations = {
+            agent_id: torch.stack(
+                [self.__flatten_as_need(torch.tensor(
+                    states[agent_id], dtype=torch.float, device=self.device))]
+            )
             for agent_id in env.agents
         }
+
         return {
-            agent_id: agent.take_action(states[agent_id], explore)
+            agent_id: agent.select_action(observations[agent_id], explore)
             for agent_id, agent in self.agents.items()
         }
 
-    def update(self, sample, agent_id):
-        obs, act, rew, next_obs, done = sample
+    def optimize(self, sample, agent_id):
+        observations, actions, rewards, next_observations, dones = sample
+
+        # centralize optimize critic
+        self.__optimize_critic(observations, actions, rewards,
+                               next_observations, dones, agent_id)
+
+        # decentralized optimize actor
+        self.__optimize_actor(observations, agent_id)
+
+    def __optimize_critic(self, observations, actions, rewards, next_observations, dones, agent_id):
         cur_agent = self.agents[agent_id]
 
+        # clear gradients
         cur_agent.critic_optimizer.zero_grad()
+
+        # calculate target values
         all_target_act = [
-            onehot_from_logits(pi(_next_obs))
-            for pi, _next_obs in zip(self.target_policies, next_obs)
+            onehot_from_logits(target_act(next_observations[agent_id])).to()
+            for agent_id, target_act in self.target_actors.items()
         ]
-        target_critic_input = torch.cat((*next_obs, *all_target_act), dim=1)
-        target_critic_value = rew[agent_id].view(
-            -1, 1) + self.gamma * cur_agent.target_critic(
-                target_critic_input) * (1 - done[agent_id].view(-1, 1))
-        critic_input = torch.cat((*obs, *act), dim=1)
-        critic_value = cur_agent.critic(critic_input)
-        critic_loss = self.critic_criterion(critic_value,
-                                            target_critic_value.detach())
-        critic_loss.backward()
+        all_next_observations = torch.cat(
+            ([obs for obs in next_observations.values()]), dim=1
+        )
+        target_input = torch.cat(
+            (all_next_observations, *all_target_act), dim=1)
+        target_values = rewards[agent_id] + \
+            self.gamma * \
+            cur_agent.target_critic(target_input) * \
+            (1 - dones[agent_id])
+
+        # calculate values
+        all_observations = torch.cat(
+            ([obs for obs in observations.values()]), dim=1
+        )
+        all_actions = torch.cat(
+            ([action for action in actions.values()]), dim=1
+        )
+        input = torch.cat((all_observations, *all_actions), dim=1)
+        values = cur_agent.critic(input)
+
+        # calculate loss
+        loss = self.critic_criterion(values,
+                                     target_values.detach())
+
+        # optimize parameters
+        loss.backward()
         cur_agent.critic_optimizer.step()
 
+    def __optimize_actor(self, observations, cur_agent_id):
+        cur_agent = self.agents[cur_agent_id]
+
+        # clear gradients
         cur_agent.actor_optimizer.zero_grad()
-        cur_actor_out = cur_agent.actor(obs[agent_id])
-        cur_act_vf_in = gumbel_softmax(cur_actor_out)
+
+        # Forward pass as if onehot (hard=True) but back-prop through a differentiable
+        # Gumbel-Softmax sample.
+        cur_actor_out = cur_agent.actor(observations[cur_agent_id])
         all_actor_acs = []
-        for i, (pi, _obs) in enumerate(zip(self.policies, obs)):
-            if i == agent_id:
+        for agent_id, actor in self.actors.items():
+            if agent_id == cur_agent_id:
+                cur_act_vf_in = gumbel_softmax(
+                    cur_actor_out, device=self.device)
                 all_actor_acs.append(cur_act_vf_in)
             else:
-                all_actor_acs.append(onehot_from_logits(pi(_obs)))
-        vf_in = torch.cat((*obs, *all_actor_acs), dim=1)
-        actor_loss = -cur_agent.critic(vf_in).mean()
-        actor_loss += (cur_actor_out**2).mean() * 1e-3
-        actor_loss.backward()
+                all_actor_acs.append(onehot_from_logits(
+                    actor(observations[agent_id])))
+
+        all_observations = torch.cat(
+            ([obs for obs in observations.values()]), dim=1
+        )
+        vf_in = torch.cat((all_observations, *all_actor_acs), dim=1)
+
+        loss = -cur_agent.critic(vf_in).mean()
+        loss += (cur_actor_out**2).mean() * 1e-3
+
+        loss.backward()
         cur_agent.actor_optimizer.step()
 
     def update_all_targets(self):
         for agt in self.agents.values():
-            agt.soft_update(agt.actor, agt.target_actor, self.tau)
-            agt.soft_update(agt.critic, agt.target_critic, self.tau)
+            agt.soft_update(self.tau)
+            agt.soft_update(self.tau)
